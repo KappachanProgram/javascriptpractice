37bb6276ddc84a265e4f0e3494a1892182555cd9
計貴 小暮 <miyamiyamiya20@yahoo.co.jp>
2022-06-16 22:44:33 +0900

    イベントを指定

--- javascript/hello.js
+++ javascript/hello.js
@@ -3,3 +3,13 @@ var list = document.querySelectorAll('#list .my');
 for(var i = 0;i < list.length;i++){
     console.log(list.item(i).href);
 }
+
+function btn_click(){
+    window.alert('ボタンをクリックしました')
+}
+
+window.onload = function(){
+    document.getElementById('btn').onclick = function(){
+        window.alert('ボタンをクリックしました');
+    };
+};
